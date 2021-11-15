54d63fd2b1d9a25b6d35dc394c836674001eb0ea
Grayson Gilbert <91498658+GraysonGilbert@users.noreply.github.com>
2021-11-15 05:25:05 +0000

    cpde

--- led8x8.py
+++ led8x8.py
@@ -9,7 +9,7 @@ dataPin, latchPin, clockPin = 23, 24, 25
 
 class LED8x8():
 
-  pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
+  #pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
   pattern1 = [0b00000000]
   pattern2 = [0b11111111]
 
@@ -17,8 +17,8 @@ class LED8x8():
     self.shifter = Shifter(data,latch,clock)
 
   def display(self):
-    self.shifter.shiftByte(LED8x8.pattern1[0])
-    self.shifter.shiftByte(LED8x8.pattern2[0]) #send second byte
+    self.shifter.shiftByte(LED8x8.pattern2[0])
+    self.shifter.shiftByte(LED8x8.pattern1[0]) #send second byte
     self.shifter.pingLatch()
  
     
