d927adbb9d0074ec7456d08fcc0a624382f30048
Grayson Gilbert <91498658+GraysonGilbert@users.noreply.github.com>
2021-11-15 21:07:06 +0000

    code

--- led8x8.py
+++ led8x8.py
@@ -4,8 +4,6 @@ from shifter import Shifter
 
 dataPin, latchPin, clockPin = 23, 24, 25
 
-#pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
-
 rows = [0b10000000, 0b01000000, 0b00100000, 0b00010000, 0b00001000, 0b00000100, 0b00000010, 0b00000001]
 
 pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001,
@@ -14,8 +12,8 @@ pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001,
 class LED8x8():
 
 
-  pattern1 = [0b00000000]
-  pattern2 = [0b11111111]
+  #pattern1 = [0b00000000]
+  #pattern2 = [0b11111111]
 
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data,latch,clock)
@@ -29,7 +27,7 @@ class LED8x8():
 
     self.shifter.pingLatch() #ping latch
     
-    time.sleep(.001)
+    time.sleep(.1)
  
     
     
